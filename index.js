5bf9e11588a1806a53f8e362773e46f8f249ab21
Tom Brennan <tommybrennan@gmail.com>
2015-09-26 08:52:24 -0400

    added the collapse event to all of the FAQs

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $("#q1").click(function() {
   $("#a1").slideToggle("slow");
+  $("#arrow1-down, #arrow1-up").toggleClass("collapse");
+})
+
+$("#q2").click(function() {
+  $("#a2").slideToggle("slow");
+  $("#arrow2-down, #arrow2-up").toggleClass("collapse");
+})
+
+$("#q3").click(function() {
+  $("#a3").slideToggle("slow");
+  $("#arrow3-down, #arrow3-up").toggleClass("collapse");
 })
