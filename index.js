1ef111a49d2d013382f55a4b48d2d10e3a358721
Tom Brennan <tommybrennan@gmail.com>
2015-09-26 09:06:58 -0400

    condensed the functionality into one event

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-$("#q1").click(function() {
-  $("#a1").slideToggle("slow");
-  $("#arrow1-down, #arrow1-up").toggleClass("collapse");
-})
-
-$("#q2").click(function() {
-  $("#a2").slideToggle("slow");
-  $("#arrow2-down, #arrow2-up").toggleClass("collapse");
-})
-
-$("#q3").click(function() {
-  $("#a3").slideToggle("slow");
-  $("#arrow3-down, #arrow3-up").toggleClass("collapse");
+$(".question").click(function() {
+  $(this).next().fadeToggle("fast");
+  $(this).children().toggleClass("collapse");
 })
